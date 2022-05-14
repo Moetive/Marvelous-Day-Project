b51b3794ae403f6989c8bbf5d9f3837355166da5
Hossein Neiman <neiman924@gmail.com>
2022-05-14 13:53:58 -0700

    have response from marvel update May 14 1:53pm

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,41 @@
+var searchBody = document.querySelector('#search-body');
+
 
 //---------------------------searchLatLon--------------------------------------------
 function searchComic() {
   //my key
-var PoKEY = "0b0978c066ab6b1971c35e90aa799998";
-var PrKey = '3d6bd3acb1691f2b4475823f3933cb22f190092f';
-// 
-var cTime = 1;
-//console.log(cTime);
+var pvtkey = "0b0978c066ab6b1971c35e90aa799998";
+var pubkey = '3d6bd3acb1691f2b4475823f3933cb22f190092f';
+
+// var ts = new Date();
+// ts = ts.getUTCMilliseconds(); 
+// console.log(ts);
 
 
 //----------------------
+//var str_1 = ts + environment.pvtkey + environment.pubkey;  
+//var str_2 = request.data["timeStamp"] + request.data["apiKey"];
+//var hash = CryptoJS.MD5(str_1).toString(); 
 
-var hash = CryptoJS.MD5(cTime+PoKEY+PrKey).toString();
+var ts = Number(new Date());
+
+var message = ts+pubkey+pvtkey;  
+var hash = CryptoJS.MD5(message).toString(); 
+
+// Create variables for date range, then put those in the URL
+var year = '1990';
+var munth = '01';
+var date1 = year + "-" + munth + "-" + '01';
+var date2 = year + "-" + munth + "-" + '31';
+var dateRange = "dateRange=" + date1 + "%2C%20" + date2;
+console.log(dateRange);
+
+QueryUrl = 'https://gateway.marvel.com/v1/public/characters?ts='+ts+'&orderBy=name&apikey='+ pvtkey +'&hash='+ hash;
+QueryUrl = "https://gateway.marvel.com:443/v1/public/comics?" + dateRange + "&format=comic&formatType=comic&dateDescriptor=thisMonth&ts=" + ts + "&apikey=" + pvtkey + "&hash=" + hash;
+
+
 console.log(hash);
-
-  var QueryUrl = "https://gateway.marvel.com:443/v1/public/comics?ts=1&format=comic&formatType=comic&dateDescriptor=thisMonth&apikey="+PoKEY+'&hash='+hash;
-  
-  console.log(QueryUrl);
-
+console.log(QueryUrl);
 
   fetch(QueryUrl)
     .then(function (response) {
@@ -27,26 +45,39 @@ console.log(hash);
       return response.json();
     })
     .then(function (locRes) {
-      // write query to page so user knows what they are viewing
-      // resultTextEl.textContent = locRes.search.query;
-      // console.log(locRes);
-      //console.log(locRes.length);
-      if (!locRes.length) {
+      console.log(locRes);
+      if (!locRes) {
         console.log('No results found!');
         resultContentEl.innerHTML = '<h3>No results found, search again!</h3>';
       } else {
-        resultContentEl.textContent = '';
-        for (var i = 0; i < locRes.length; i++) {
-          
-          lat = locRes[i].lat;
-          lon = locRes[i].lon;
-          console.log('Lat = ',lat,'lon = ' , lon);
-
-          searchWeather(lat,lon);
+        var newObj = locRes['data']['results'];
+        console.log(newObj);
+        for (var i = 0; i < newObj.length; i++) {
+          printout(newObj[i]['characters']['items']);
         }
       }
     })
     .catch(function (error) {
       console.error(error);
     });
+}
+
+function printout(items){
+  for (var i = 0; i < items.length; i++) {
+    searchHistory(items[i]['name']);
+  }
+}
+
+
+//------------------------------search history-------------------------------------------
+function searchHistory(resultObj) {
+  console.log(resultObj);
+      var linkButtonEl = document.createElement('a');
+      //https://www.google.com/search?q=fantastic+four
+      var url = 'https://www.google.com/search?q=' + resultObj+ ' comic book marvel';
+      linkButtonEl.textContent = resultObj;
+      linkButtonEl.setAttribute('id',resultObj);
+      linkButtonEl.setAttribute('href', url);
+      linkButtonEl.classList.add('btn', 'btn-dark','column','margin','btn-info', 'btn-block');
+      searchBody.prepend(linkButtonEl);
 }
