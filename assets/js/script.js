0d0e8129822e6345260d3c62a8fe6113953116cb
dhuw <hazlewood.dyl@gmail.com>
2022-05-19 20:31:27 -0700

    adding wikiAPI fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -277,7 +277,23 @@ f();
 searchComic();
 
 // wiki API additions
+var url = "https://en.wikipedia.org/w/api.php"; 
 
+var params = {
+    action: "opensearch",
+    search: "Spider Man" + " Comic",
+    limit: "5",
+    namespace: "0",
+    format: "json"
+};
+
+url = url + "?origin=*";
+Object.keys(params).forEach(function(key){url += "&" + key + "=" + params[key];});
+
+fetch(url)
+    .then(function(response){return response.json();})
+    .then(function(response) {console.log(response);})
+    .catch(function(error){console.log(error);});
 
 
 
