f090139ef11218afcd7378abb4c2f481242f8e42
chicken1991 <77861966+chicken1991@users.noreply.github.com>
2022-05-20 22:03:02 -0700

    displayCard modal function displays stuff

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,30 +11,30 @@ var wikiObj;
 //---------------------------search Comic--------------------------------------------
 function searchComic() {
   //my key
-getParams();
-document.body.style.background.opacity = '0.1';
+  getParams();
+  document.body.style.background.opacity = '0.1';
 
 
-var pvtkey = "0b0978c066ab6b1971c35e90aa799998";
-var pubkey = '3d6bd3acb1691f2b4475823f3933cb22f190092f';
+  var pvtkey = "0b0978c066ab6b1971c35e90aa799998";
+  var pubkey = '3d6bd3acb1691f2b4475823f3933cb22f190092f';
 
-var ts = Number(new Date());
+  var ts = Number(new Date());
 
-var message = ts+pubkey+pvtkey;  
-var hash = CryptoJS.MD5(message).toString(); 
+  var message = ts + pubkey + pvtkey;
+  var hash = CryptoJS.MD5(message).toString();
 
-// Create variables for date range, then put those in the URL
+  // Create variables for date range, then put those in the URL
 
-var date1 = year + "-" + munth + "-" + '01';
-var date2 = year + "-" + munth + "-" + '31';
-var dateRange = "dateRange=" + date1 + "%2C%20" + date2;
-console.log(dateRange);
+  var date1 = year + "-" + munth + "-" + '01';
+  var date2 = year + "-" + munth + "-" + '31';
+  var dateRange = "dateRange=" + date1 + "%2C%20" + date2;
+  console.log(dateRange);
 
-// QueryUrl = 'https://gateway.marvel.com/v1/public/characters?ts='+ts+'&orderBy=name&apikey='+ pvtkey +'&hash='+ hash;
-QueryUrl = "https://gateway.marvel.com:443/v1/public/comics?" + dateRange + "&format=comic&formatType=comic&dateDescriptor=thisMonth&ts=" + ts + "&apikey=" + pvtkey + "&hash=" + hash;
+  // QueryUrl = 'https://gateway.marvel.com/v1/public/characters?ts='+ts+'&orderBy=name&apikey='+ pvtkey +'&hash='+ hash;
+  QueryUrl = "https://gateway.marvel.com:443/v1/public/comics?" + dateRange + "&format=comic&formatType=comic&dateDescriptor=thisMonth&ts=" + ts + "&apikey=" + pvtkey + "&hash=" + hash;
 
-console.log(hash);
-console.log(QueryUrl);
+  console.log(hash);
+  console.log(QueryUrl);
 
   fetch(QueryUrl)
     .then(function (response) {
@@ -53,7 +53,13 @@ console.log(QueryUrl);
         console.log(newObj);
         for (var i = 0; i < newObj.length; i++) {
           // printout(newObj[i]['characters']['items']);
-          searchHistory(newObj[i]['title'],newObj[i]['thumbnail']['path'],newObj[i]['urls'][0]['url']);
+          var histTitle = newObj[i]['title'];
+          var histThumbnail = newObj[i]['thumbnail']['path'] + ".jpg";
+          console.log(histThumbnail);
+          var histMarvelUrl = newObj[i]['urls'][0]['url'];
+          var histDate = moment(newObj[i]['dates'][0]['date']).format("MMM Do, YYYY");
+          console.log(histDate);
+          searchHistory(histTitle, histThumbnail, histMarvelUrl, histDate);
         }
       }
     })
@@ -69,75 +75,83 @@ console.log(QueryUrl);
 function searchWiki(resultObj) {
   var wikiTitle;
   var nameCleaned = resultObj.replace(/\s/g, '%20')
-  var value = "'"+nameCleaned+"'"
+  var value = "'" + nameCleaned + "'"
   console.log(value);
-  var WikiQueryUrl = 'https://en.wikipedia.org/w/rest.php/v1/search/page?limit=1&q='+value;
+  var WikiQueryUrl = 'https://en.wikipedia.org/w/rest.php/v1/search/page?limit=1&q=' + value;
   //console.log(WikiQueryUrl);
-  
-    fetch(WikiQueryUrl)
-      .then(function (response) {
-        if (!response.ok) {
-          throw response.json();
-        }
-        return response.json();
-      })
-      .then(function (locRes) {
-        if (!locRes) {
-          console.log('No results found!');
-          resultContentEl.innerHTML = '<h3>No results found, search again!</h3>';
-        } else {
-          console.log(locRes);
-          wikiObj = locRes['pages'][0]['title'];
-          console.log(wikiObj);
-          return wikiTitle;  
-        }
 
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
-      // wikiTitle = locRes['pages'][0]['description'];
-    return wikiTitle;
-  }
-  
+  fetch(WikiQueryUrl)
+    .then(function (response) {
+      if (!response.ok) {
+        throw response.json();
+      }
+      return response.json();
+    })
+    .then(function (locRes) {
+      if (!locRes) {
+        console.log('No results found!');
+        resultContentEl.innerHTML = '<h3>No results found, search again!</h3>';
+      } else {
+        console.log(locRes);
+        wikiObj = locRes['pages'][0]['title'];
+        console.log(wikiObj);
+        return wikiTitle;
+      }
+
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+  // wikiTitle = locRes['pages'][0]['description'];
+  return wikiTitle;
+}
+
 //Ben's indexing var
 var idIndex = 0;
-
-//Ben's modal el
-var modalEl = $("#comicDetails");
 //------------------------------search history-------------------------------------------
-function searchHistory(resultObj,path,Marvelurl) {
-      var wikiTitle = searchWiki(resultObj);
-      console.log(wikiTitle);
-      var p1 = document.createElement('p');
-      var linkButtonEl = document.createElement('a');
-      //https://www.google.com/search?q=fantastic+four
-      var url = 'https://www.google.com/search?q=' + resultObj+ ' comic book marvel';
-      linkButtonEl.textContent = resultObj;
-      p1.setAttribute('id','p1');
-  //  linkButtonEl.setAttribute('href', path+'.jpg', target = "_self");
-      // linkButtonEl.setAttribute('href',Marvelurl, target = "_self");
-      linkButtonEl.setAttribute('title',wikiTitle);
-      linkButtonEl.setAttribute('id', idIndex);
-      linkButtonEl.classList.add('btn', 'btn-dark','column','margin','btn-info', 'btn-block');
-      searchBody.append(p1);
-      p1.appendChild(linkButtonEl);
-      // b.getAttribute("href") && b.hostname !== location.hostname && (b.target = "_blank") } } ; externalLinks();  
-      linkButtonEl.addEventListener('click', function (event) {
-        var btnID = event.target.id;
-        //var inputText = "what ever just testing"  // document.getElementById(btnIndex).value; //wtf is this
-        //based on index above, save the input text to an object
-        $("#detail-body").text(resultObj);
-        modalEl.modal("toggle");
-      });    
-      idIndex++;
-      console.log(modalEl.innerHTML);
+function searchHistory(resultObj, path, Marvelurl, dates) {
+  var wikiTitle = searchWiki(resultObj);
+  console.log(wikiTitle);
+  var p1 = document.createElement('p');
+  var linkButtonEl = document.createElement('a');
+  //https://www.google.com/search?q=fantastic+four
+  var url = 'https://www.google.com/search?q=' + resultObj + ' comic book marvel';
+  linkButtonEl.textContent = resultObj;
+  p1.setAttribute('id', 'p1');
 
+  // BP commented these out so I could see modal happen
+  //  linkButtonEl.setAttribute('href', path+'.jpg', target = "_self");
+  // linkButtonEl.setAttribute('href',Marvelurl, target = "_self");
+
+  var storedMarvelUrl = Marvelurl;
+  var storedThumbnail = path;
+  var storedDates = dates;
+
+  linkButtonEl.setAttribute('title', resultObj);
+  linkButtonEl.setAttribute('id', idIndex);
+  linkButtonEl.classList.add('btn', 'btn-dark', 'column', 'margin', 'btn-info', 'btn-block');
+  searchBody.append(p1);
+  p1.appendChild(linkButtonEl);
+  // b.getAttribute("href") && b.hostname !== location.hostname && (b.target = "_blank") } } ; externalLinks();  
+  linkButtonEl.addEventListener('click', function (event) {
+    var btnID = event.target.id;
+    displayCard(resultObj, storedThumbnail, storedMarvelUrl, storedDates);
+  //   $("#detail-body").text(resultObj).addClass("bg-dark text-white");
+  //   //List things from wiki
+  //   for(var i = 0; i < 5; i++){
+  //     // var wikiLink = wikiSeach(resultObj).
+  //     // $("#detail-body").
+  //   }
+  //   modalEl.modal("toggle");
+  });
+  // idIndex++;
+  // console.log(modalEl.innerHTML);
+  // wikiSearch(resultObj);
 }
 
 //------------------------------function printout-------------------------------------------
 
-function printout(items){
+function printout(items) {
   for (var i = 0; i < items.length; i++) {
     searchHistory(items[i]['name']);
   }
@@ -150,20 +164,20 @@ var munthText = document.getElementById("munthText");
 // function handlemunthPick(event) {
 //    console.log(document.getElementById("date").value);
 // }
-$(document).on("click", ".myclass figure", function() {
+$(document).on("click", ".myclass figure", function () {
   var year = document.getElementById("date").value;
   console.log(year);
   var munth = $('i', this).length ? $('i', this).text() : $(this).text();
-  var url = './search-result.html?year=' + year+'&munth=' + munth;
+  var url = './search-result.html?year=' + year + '&munth=' + munth;
   console.log(url);
-  document.location= url,'_blank';
+  document.location = url, '_blank';
 });
 //----------------------------get Params from url------------------------------------------
 function getParams() {
   var searchParamsArr = document.location.search.split('&');
   year = searchParamsArr[0].split('=').pop();
   munth = searchParamsArr[1].split('=').pop();
-  console.log(year,munth);
+  console.log(year, munth);
   return;
   // Get the query and format values
   //cityName = searchParamsArr[0].split('=').pop();
@@ -171,7 +185,7 @@ function getParams() {
   //searchApi(query, format);
 }
 //---------------------------------------------------------------------------
-$(function() {
+$(function () {
   $('#p1 a').miniPreview({ prefetch: 'pageload' });
 });
 //---------------------------background effect------------------------------
@@ -187,7 +201,7 @@ canvas.height = canvas.offsetHeight;
 
 const gl = canvas.getContext('webgl2');
 if (!gl) {
-    alert('require webgl 2.0, bye')
+  alert('require webgl 2.0, bye')
 }
 
 const vss = `#version 300 es
@@ -218,16 +232,16 @@ const vs = gl.createShader(gl.VERTEX_SHADER);
 gl.shaderSource(vs, vss);
 gl.compileShader(vs);
 if (!gl.getShaderParameter(vs, gl.COMPILE_STATUS)) {
-    console.error(gl.getShaderInfoLog(vs));
-    throw 1;
+  console.error(gl.getShaderInfoLog(vs));
+  throw 1;
 }
 
 const fs = gl.createShader(gl.FRAGMENT_SHADER);
 gl.shaderSource(fs, fss);
 gl.compileShader(fs);
 if (!gl.getShaderParameter(fs, gl.COMPILE_STATUS)) {
-    console.error(gl.getShaderInfoLog(fs));
-    throw 2;
+  console.error(gl.getShaderInfoLog(fs));
+  throw 2;
 }
 
 const prg = gl.createProgram();
@@ -235,8 +249,8 @@ gl.attachShader(prg, vs);
 gl.attachShader(prg, fs);
 gl.linkProgram(prg);
 if (!gl.getProgramParameter(prg, gl.LINK_STATUS)) {
-    console.error(gl.getProgramInfoLog(prg));
-    throw 3;
+  console.error(gl.getProgramInfoLog(prg));
+  throw 3;
 }
 
 gl.detachShader(prg, vs);
@@ -255,17 +269,17 @@ gl.bindVertexArray(va);
 const N = 300; // n triangles
 
 let ps;
-{    
-    ps = new Float32Array(2 + N * 2 * 2);
-    ps[0] = 0; // clip space center
-    ps[1] = 0;
-    let j = 2;
-    for (let i = 0; i < N; ++i) {
-        ps[j++] = Math.random() * 2 - 1; //x 
-        ps[j++] = Math.random() * 2 - 1; //y
-        ps[j++] = Math.random() * 2 - 1; //x 
-        ps[j++] = Math.random() * 2 - 1; //y
-    }
+{
+  ps = new Float32Array(2 + N * 2 * 2);
+  ps[0] = 0; // clip space center
+  ps[1] = 0;
+  let j = 2;
+  for (let i = 0; i < N; ++i) {
+    ps[j++] = Math.random() * 2 - 1; //x 
+    ps[j++] = Math.random() * 2 - 1; //y
+    ps[j++] = Math.random() * 2 - 1; //x 
+    ps[j++] = Math.random() * 2 - 1; //y
+  }
 }
 
 const buf = gl.createBuffer();
@@ -273,22 +287,22 @@ gl.bindBuffer(gl.ARRAY_BUFFER, buf);
 gl.bufferData(gl.ARRAY_BUFFER, ps, gl.DYNAMIC_DRAW);
 gl.enableVertexAttribArray($p);
 gl.vertexAttribPointer(
-    $p,
-    2, gl.FLOAT, // two 32b-float (8bytes)
-    false,
-    0, // skip n byte to fetch next
-    0  // skip n byte to fetch first
+  $p,
+  2, gl.FLOAT, // two 32b-float (8bytes)
+  false,
+  0, // skip n byte to fetch next
+  0  // skip n byte to fetch first
 );
 
-let idxs; 
-{ 
-    idxs = new Uint16Array(3 * N);
-    let j = 0;
-    for (let i = 0; i < N; ++i) {
-        idxs[j++] = 0;
-        idxs[j++] = 1 + i * 2;
-        idxs[j++] = 2 + i * 2;
-    }
+let idxs;
+{
+  idxs = new Uint16Array(3 * N);
+  let j = 0;
+  for (let i = 0; i < N; ++i) {
+    idxs[j++] = 0;
+    idxs[j++] = 1 + i * 2;
+    idxs[j++] = 2 + i * 2;
+  }
 }
 
 const ibuf = gl.createBuffer();
@@ -310,42 +324,83 @@ gl.bindVertexArray(va);
 
 
 function f() {
-    gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.uniform4fv($c, [0.2, 0.2, 0.2, 0.02]);
-    gl.drawElements(
-        gl.TRIANGLES,
-        idxs.length, // n indices
-        gl.UNSIGNED_SHORT, // ui16
-        0 // skip n bytes to fetch first
-    );
+  gl.clear(gl.COLOR_BUFFER_BIT);
+  gl.uniform4fv($c, [0.2, 0.2, 0.2, 0.02]);
+  gl.drawElements(
+    gl.TRIANGLES,
+    idxs.length, // n indices
+    gl.UNSIGNED_SHORT, // ui16
+    0 // skip n bytes to fetch first
+  );
 }
 f();
 
 // ---
 gl.bindBuffer(gl.ARRAY_BUFFER, buf);
 document.body.onmousemove = (e) => {
-    ps[0] = e.clientX / window.innerWidth * 2 - 1;
-    ps[1] = -1 * (e.clientY / window.innerHeight * 2 - 1);
-    gl.bufferSubData(gl.ARRAY_BUFFER, 0, ps.slice(0, 2)); // that's why DYNAMIC_DRAW
-    f();
-} 
+  ps[0] = e.clientX / window.innerWidth * 2 - 1;
+  ps[1] = -1 * (e.clientY / window.innerHeight * 2 - 1);
+  gl.bufferSubData(gl.ARRAY_BUFFER, 0, ps.slice(0, 2)); // that's why DYNAMIC_DRAW
+  f();
+}
 
-// Wiki API stuff
+// Wiki API stuff!
+async function wikiSearch(title) {
+  var url = "https://en.wikipedia.org/w/api.php";
 
-var url = "https://en.wikipedia.org/w/api.php"; 
-
-var params = {
+  var params = {
     action: "opensearch",
-    search: "Spider Man" + " Comic",
+    search: title.trim(),
     limit: "5",
     namespace: "0",
     format: "json"
-};
+  };
 
-url = url + "?origin=*";
-Object.keys(params).forEach(function(key){url += "&" + key + "=" + params[key];});
+  url = url + "?origin=*";
+  Object.keys(params).forEach(function (key) { url += "&" + key + "=" + params[key]; });
 
-fetch(url)
-    .then(function(response){return response.json();})
-    .then(function(response) {console.log(response);})
-    .catch(function(error){console.log(error);});
+  fetch(url)
+    .then(function (response) { 
+      return response.json(); 
+    })
+    .then(function (response) { 
+      console.log(response); 
+    })
+    .catch(function (error) {
+       console.log(error); 
+      });
+      console.log(url);
+  
+
+}
+
+var modalEl = $("#comicDetails");
+//Display the card stuff and junk?
+function displayCard(title, image, links, date){
+  if(modalDate){
+  modalDate.empty();
+  }
+  var theLinks = links;
+  var modalTitle = $(".modal-title");
+  var modalBody = $(".modal-body");
+  var modalDate = $("<h5>");
+  var modalLinks = $("<p>");
+  var modalImage = $("<img>");
+  modalTitle.text(title).addClass("bg-dark text-white");
+  
+  modalDate.text("Released on: " + date);
+  modalBody.append(modalDate);
+console.log(image + "------------------------------------------------");
+  modalImage.attr("src", image);
+  modalImage.attr("id", "detail-img");
+  modalBody.append(modalImage);
+    //List things from wiki
+      // var wikiLink = wikiSeach(resultObj).
+      // $("#detail-body").
+      // modalLinks.text(wikiSearch(title)[3].i);
+      // modalBody.append(modalLinks);
+      console.log(wikiSearch(title)[3]);
+    modalEl.modal("toggle");
+  idIndex++;
+  
+  }
